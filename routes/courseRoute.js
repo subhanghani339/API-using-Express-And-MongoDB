ce2a719b45febbcad0373b0a41b31bc70a7f6288
subhanghani339 <subhanghani339@gmail.com>
2023-05-23 03:05:17 +0500

    Updated

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,22 +1,60 @@
 const express = require("express");
 const CoursesModel = require("../models/courseModels");
 const { SendResponse } = require("../helper/helper");
-const route = express.Router()
+const route = express.Router();
 
 route.get("/", async (req, res) => {
-    try {
-      const result = await CoursesModel.find();
-      if (!result) {
-        res.send(SendResponse(false, null, "Data Not Found")).status(404);
-      } else {
-        res.send(SendResponse(true, result)).status(200);
-      }
-    } catch (e) {
-      res.send(SendResponse(false, null, "Internal Server Error")).status(400);
+  try {
+    const result = await CoursesModel.find();
+    if (!result) {
+      res.send(SendResponse(false, null, "Data Not Found")).status(404);
+    } else {
+      res.send(SendResponse(true, result)).status(200);
     }
-  });
+  } catch (e) {
+    res.send(SendResponse(false, null, "Internal Server Error")).status(400);
+  }
+});
 
-route.get('/:id',(req,res)=>{});
-route.post('/',(req,res)=>{});
-route.put('/:id',(req,res)=>{});
-route.delete('/:id',(req,res)=>{});
+route.get("/:id", (req, res) => {});
+
+route.post("/", async (req, res) => {
+  const { courseName, courseDuration, courseFees, courseShortName } = req.body;
+  try {
+    if (!courseName) {
+      res.send(SendResponse(false, null, "Course Name Required"));
+    }
+
+    if (!courseDuration) {
+      res.send(SendResponse(false, null, "Course Duration Required"));
+    }
+
+    if (!courseFees) {
+      res.send(SendResponse(false, null, "Course Fees Required"));
+    }
+
+    if (!courseShortName) {
+      res.send(SendResponse(false, null, "Course Short Name Required"));
+    } 
+    
+    else {
+      let Obj = { courseName, courseDuration, courseFees, courseShortName };
+      let course = new CoursesModel(Obj);
+      await course.save();
+      if (!course) {
+        res
+          .send(SendResponse(false, null, "Internal Server Error"))
+          .status(400);
+      } else {
+        res
+          .send(SendResponse(true, student, "Data Saved Successfully"))
+          .status(200);
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+route.put("/:id", (req, res) => {});
+route.delete("/:id", (req, res) => {});
